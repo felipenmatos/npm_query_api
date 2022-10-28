16865a668fc62e1dc2f65b5808a0aad863efefe1
felipenmatos <notfelipe2@hotmail.com>
2022-10-28 17:02:11 -0300

    feat: server creation and query endpoint implementation using axios and express

---
+++ index.js
@@ -0,0 +1,15 @@
+const axios = require("axios");
+const express = require("express");
+
+const app = express();
+
+app.get("/descrever/:pacote", async (req, res) => {
+  const pacote = req.params.pacote;
+  const response = await axios.get(`https://registry.npmjs.com/${pacote}`);
+
+  res.json({
+    descricao: response.data.description,
+  });
+});
+
+app.listen(8080);
